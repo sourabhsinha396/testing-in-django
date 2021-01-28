3f144d47bfd03e1a0b3713257114630591fcd7c8
Superuser <ilovedjango3000@gmail.com>
2021-01-29 01:15:12 +0530

    Add models for jobs

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,1 +1,26 @@
 from django.contrib import admin
+
+from .models import Role, Location, Job
+
+
+class RoleAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Role, RoleAdmin)
+
+
+class LocationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+
+admin.site.register(Location, LocationAdmin)
+
+
+class JobAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ["title", "publish"]
+    search_fields = ["title", "job_description"]
+
+
+admin.site.register(Job, JobAdmin)
--- jobs/models.py
+++ jobs/models.py
@@ -1,1 +1,30 @@
 from django.db import models
+
+
+class Role(models.Model):
+    title = models.CharField(max_length=80)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+
+class Job(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField()
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
+    experience = models.PositiveSmallIntegerField()
+    salary = models.CharField(max_length=50, blank=True, null=True)
+    job_description = models.TextField(blank=True, null=True)
+    responsibility = models.TextField(blank=True, null=True)
+    others = models.TextField(blank=True, null=True)
+    publish = models.BooleanField(default=False)
